487df9d694deb7d907861eb9421a22db477d0e2f
GeheimagentNr1 <geheimelache@hotmail.com>
2026-08-24 20:27:46 +0200

    feat: scaffold vatsim-stats multi-module Maven project

---
+++ app/src/test/java/de/secretsoft/vatsim_stats/VatsimStatsApplicationTests.java
@@ -0,0 +1,12 @@
+package de.secretsoft.vatsim_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VatsimStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
